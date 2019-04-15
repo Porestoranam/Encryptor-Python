6b9028d2fc93ceaac9be27f2a4efb5807d6cf6fa
Porestoranam <mazheikame@mail.ru>
2019-04-15 18:01:38 +0300

    made short code

--- encode_and_decode.py
+++ encode_and_decode.py
@@ -1,14 +1,10 @@
 def new_char(char, shift):
     """make new char using value of shift"""
     ord_char = ord(char)
-    dis_from_cap_a = ord_char - ord('A')
-    dis_from_cap_z = ord('Z') - ord_char
-    dis_from_a = ord_char - ord('a')
-    dis_from_z = ord('z') - ord_char
-    if (dis_from_cap_a >= 0) and (dis_from_cap_z >= 0):
+    if (ord_char - ord('A') >= 0) and (ord('Z') - ord_char >= 0):
         new_ord = (ord_char - ord('A') + shift) % 26 + ord('A')
         return chr(new_ord)
-    elif (dis_from_a >= 0) and (dis_from_z >= 0):
+    elif (ord_char - ord('a') >= 0) and (ord('z') - ord_char >= 0):
         new_ord = (ord_char - ord('a') + shift) % 26 + ord('a')
         return chr(new_ord)
     else:
